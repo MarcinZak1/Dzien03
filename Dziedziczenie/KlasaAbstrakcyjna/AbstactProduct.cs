33097d5d00c8d676e8c1e11b7248085e1cf884e7
Marcin Żak <marcinzak1992@gmail.com>
2020-04-25 17:01:28 +0200

    major changes

---
+++ Dziedziczenie/KlasaAbstrakcyjna/AbstactProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziedziczenie.KlasaAbstrakcyjna
+{
+    abstract class AbstactProduct
+    {
+        protected string name;
+        protected double price;
+
+        public AbstactProduct(string n, double p)
+        {
+            name = n;
+            price = p;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Info o produkcie {0} - {1}", name, price);
+        }
+
+        public abstract void ShowPrice();
+    }
+}
---
+++ MiniShop/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop
+{
+    public class Product
+    {
+        private int id;
+        private String name;
+        private double price;
+        private String descr = String.Empty;        // = ""
+        private UInt16 version = 0;
+        private bool promo = false;
+        private bool active = true;
+
+        public double Price { get { return price; } }
+
+        // ustawianie parametrow
+        public void SetParam(int id, string name, double price, string descr="")
+        {
+
+        }
+        // zmiana ceny
+        public void ChangePrice(double newPrice)
+        {
+            // zastanowic się nad mechanizmem logowania historii zmian cen
+        }
+        // zmiana opisu
+        public void ChangeDescription(String newDescr)
+        {
+
+        }
+        public void SetActive(bool isActive)
+        {
+            this.active = isActive;
+        }
+    }
+}
