8d129be6e2eb54acf60d49d718eddf46ff474393
MarcinZak1 <64193242+MarcinZak1@users.noreply.github.com>
2020-04-25 14:26:20 +0200

    Update Okrag.cs

--- Dziedziczenie/Figury/Okrag.cs
+++ Dziedziczenie/Figury/Okrag.cs
@@ -4,10 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//zmiana z poziomu GitHuba
 namespace Dziedziczenie.Figury
 {
     class Okrag
     {
         private double srednica;
+        
+        public double ObliczPole()
+        {
+            return 3.14159* promien*promiec;
+        }
     }
 }
