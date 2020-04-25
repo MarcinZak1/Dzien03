aef08c211963b33c4d50c720a45a3211a21b2964
Marcin Żak <marcinzak1992@gmail.com>
2020-04-25 14:08:43 +0200

    initial commit

---
+++ Dziedziczenie/Figury/Kwadrat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziedziczenie.Figury
+{
+    class Kwadrat : Prostokat
+    {
+        public Kwadrat(double bok) : base(bok, bok)         // dziedziczenie konstruktora 
+        {
+            Console.WriteLine("Kontruktor klasy Kwadrat");
+        }
+
+        public bool CzyKwadrat()
+        {
+            return base.CzyKwadrat();
+        }
+    }
+}
---
+++ Dziedziczenie/Figury/Prostokat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziedziczenie.Figury
+{
+    class Prostokat : Figura, IFigura, IFiguraStat
+    {
+        private double bokA;
+        private double bokB;
+
+        public Prostokat(double bokA, double bokB)
+        {
+            this.bokA = bokA;
+            this.bokB = bokB;
+            liczbaBokow = 2;
+        }
+
+        /*public double ObliczPole()
+        {
+            return bokA * bokB;
+        }*/
+
+        public bool CzyKwadrat()
+        {
+            return bokA == bokB;
+        }
+
+        public double ObliczObwod()
+        {
+            return 2 * bokA + 2 * bokB;
+        }
+
+        public double ObliczPole()
+        {
+            return bokA * bokB;
+        }
+
+        public int PodajLiczbeBokow()
+        {
+            return liczbaBokow;
+        }
+    }
+}
